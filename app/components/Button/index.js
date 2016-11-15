9bbcfbe5e028e20bb03a29c6c455ebeb8b305f52
Aldren Terante <aldren.terante@salarium.com>
2016-11-15 15:47:03 +0800

    update Button component

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -4,60 +4,38 @@ import styles from './styles.css';
 
 /**
 * Button
-*
-  let button =
-  <button
-    className={className}
-    href={props.href}
-    onClick={props.onClick}
-  >
-    {props.children}
-  </button>;
-
-  if (props.handleRoute) {
-    button =
-      <button
-      className={className}
-      onClick={props.handleRoute}
-    >
-      {props.children}
-    </button>;
-  }
 */
+function Button( props ) {
+    const className = cx( styles.base, {
+        [ styles.sizeSmall ]: props.size === 'small',
+        [ styles.sizeLarge ]: props.size === 'large',
+        [ styles.primary ]: props.type === 'primary',
+        [ styles.primaryOutline ]: props.type === 'primary' && props.outline
+    });
 
-function Button(props) {
-  const className = cx(styles.base, {
-    [styles.sizeSmall]: (props.size === 'small'),
-    [styles.sizeLarge]: (props.size === 'large'),
-    [styles.primary]: (props.type === 'primary'),
-    [styles.primaryOutline]: (props.type === 'primary') && props.outline,
-  });
-
-  return (
-    <button
-      className={className}
-      href
-      handleRoute
-      onClick={props.onClick}
-    >
-      {props.children}
-    </button>
-  );
+    return (
+        <button
+            className={ className }
+            onClick={ props.onClick }
+        >
+            { props.children }
+        </button>
+    );
 }
 
 Button.propTypes = {
-  size: React.PropTypes.string,
-  outline: React.PropTypes.bool,
-  type: React.PropTypes.string,
-  onClick: React.PropTypes.func,
-  children: React.PropTypes.node,
+    size: React.PropTypes.string,
+    outline: React.PropTypes.bool,
+    type: React.PropTypes.string,
+    onClick: React.PropTypes.func,
+    children: React.PropTypes.node
 };
 
 Button.defaultProps = {
-  size: 'default',
-  outline: false,
-  type: 'default',
-  children: 'Default Button',
+    size: 'default',
+    outline: false,
+    type: 'default',
+    children: 'Default Button'
 };
 
 export default Button;
--- app/components/Button/tests/index.test.js
+++ app/components/Button/tests/index.test.js
@@ -3,24 +3,25 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 
-describe('<Button />', () => {
-  it('renders a <button>', () => {
-    const component = shallow(
-      <Button></Button>
-    );
-    expect(
-      component.find('button').node
-    ).toExist();
-  });
+describe( '<Button />', () => {
+    it( 'renders a <button>', () => {
+        const component = shallow(
+            <Button></Button>
+        );
+        expect(
+            component.find( 'button' ).node
+        ).toExist();
+    });
 
-  it('should have default prototype', () => {
-    const component = shallow(
-      <Button></Button>
-    );
-    const props = component.instance().props;
-    expect(props.size).toEqual('default');
-    expect(props.outline).toBe(false);
-    expect(props.type).toEqual('default');
-    expect(props.children).toEqual('Default Button');
-  });
+    it( 'should have default prototype', () => {
+        const component = shallow(
+            <Button></Button>
+        );
+        const props = component.instance().props;
+
+        expect( props.size ).toEqual( 'default' );
+        expect( props.outline ).toBe( false );
+        expect( props.type ).toEqual( 'default' );
+        expect( props.children ).toEqual( 'Default Button' );
+    });
 });
