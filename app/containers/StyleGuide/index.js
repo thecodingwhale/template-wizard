0da7c6fb20abca81758751b033987fd1edd06b25
Aldren Terante <aldren.terante@salarium.com>
2016-11-15 15:41:34 +0800

    remove unsed containers and test cases

--- app/containers/StyleGuide/index.js
+++ app/containers/StyleGuide/index.js
@@ -1,47 +1,121 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import styles from './styles.css';
 import Button from  '../../components/Button'; // eslint-disable-line no-multi-spaces
+import TopBar from  '../../components/TopBar'; // eslint-disable-line no-multi-spaces
 
-export class StyleGuide extends React.Component {
-  renderButtons() {
-    return (
-      <div>
-        <h1 className={styles.pageTitle}>Buttons</h1>
-        <p>
-          <Button />
-          <Button type="primary">Primary Button</Button>
-          <Button type="primary" outline>Outline Primary Button</Button>
-        </p>
-        <p>
-          <Button size="small" outline>Small Button</Button>
-          <Button type="primary">Default Button</Button>
-          <Button type="primary" size="large" outline />
-        </p>
-      </div>
-    );
-  }
-  render() {
-    return (
-      <div className={styles.styleGuide}>
-        <Helmet
-          title="StyleGuide"
-          meta={[
-            { name: 'description', content: 'Description of StyleGuide' },
-          ]}
-        />
-        {this.renderButtons()}
-      </div>
-    );
-  }
+/**
+* StyleGuide
+*/
+export default class StyleGuide extends React.Component {
+    /**
+    * renderTopBar()
+    */
+    renderTopBar() {
+        return (
+            <div>
+                <div className={ styles.content }>
+                    <h1 className={ styles.pageTitle }>
+                        Top Bar
+                    </h1>
+                    <div className={ styles.section }>
+                        <TopBar title="Template Wizard">
+                            <Button type="primary" outline size="large">
+                                PDF PREVIEW
+                            </Button>
+                            <Button type="primary" size="large">
+                                SAVE
+                            </Button>
+                        </TopBar>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    /**
+    * renderButtons()
+    */
+    renderButtons() {
+        return (
+            <div className={ styles.content }>
+                <h1 className={ styles.pageTitle }>Buttons</h1>
+                <div className={ styles.section }>
+                    <p>
+                        <Button />
+                        <Button type="primary">Primary Button</Button>
+                        <Button type="primary" outline>Outline Primary Button</Button>
+                    </p>
+                    <p>
+                        <Button size="small" outline>Small Button</Button>
+                        <Button type="primary">Default Button</Button>
+                        <Button type="primary" size="large" outline />
+                    </p>
+                </div>
+            </div>
+        );
+    }
+    /**
+    * renderButtonLists()
+    */
+    renderButtonLists() {
+        return (
+            <div className={ styles.content }>
+                <h1 className={ styles.pageTitle }>Payslip Button Option Lists</h1>
+                <div className={ styles.section }>
+                    <div className={ styles.blockContent }>
+                        <div className={ styles.optionTitle }>
+                            Employee Details
+                        </div>
+                        <div className={ styles.buttonGroup }>
+                            <Button type="primary">Employee Name</Button>
+                            <Button type="primary">Employee ID</Button>
+                            <Button type="primary">Gender</Button>
+                            <Button type="primary">Photo</Button>
+                            <Button>Birth Date</Button>
+                            <Button>Mobile</Button>
+                            <Button>Telephone</Button>
+                            <Button>Email</Button>
+                            <Button>Address</Button>
+                            <Button>Zip</Button>
+                        </div>
+                    </div>
+                    <div className={ styles.blockContent }>
+                        <div className={ styles.optionTitle }>
+                            Employee Details
+                        </div>
+                        <div className={ styles.buttonGroup }>
+                            <Button type="primary">Employee Name</Button>
+                            <Button type="primary">Employee ID</Button>
+                            <Button type="primary">Gender</Button>
+                            <Button type="primary">Photo</Button>
+                            <Button>Birth Date</Button>
+                            <Button>Mobile</Button>
+                            <Button>Telephone</Button>
+                            <Button>Email</Button>
+                            <Button>Address</Button>
+                            <Button>Zip</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    /**
+    * render()
+    */
+    render() {
+        return (
+            <div className={ styles.styleGuide }>
+                <Helmet
+                    title="StyleGuide"
+                    meta={ [
+                    { name: 'description', content: 'Description of StyleGuide' }
+                    ] }
+                />
+                { this.renderTopBar() }
+                { this.renderButtons() }
+                { this.renderButtonLists() }
+            </div>
+        );
+    }
 }
-
-
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-export default connect(null, mapDispatchToProps)(StyleGuide);
