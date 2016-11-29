1fe6f045db8aba10888a060246ed6a6839f91f9a
Aldren Terante <aldren.terante@salarium.com>
2016-11-29 17:19:48 +0800

    move all action type to constant

--- app/containers/TemplateWizard/constants.js
+++ app/containers/TemplateWizard/constants.js
@@ -3,3 +3,7 @@
  */
 
 export const DEFAULT_ACTION = 'app/TemplateWizard/DEFAULT_ACTION';
+export const LOAD_TEMPLATES = 'app/TemplateWizard/LOAD_TEMPLATES';
+export const TEMPLATES_LOADED = 'app/TemplateWizard/TEMPLATES_LOADED';
+export const SAVE_TEMPLATE = 'app/TemplateWizard/SAVE_TEMPLATE';
+export const NEW_TEMPLATE_ADDED = 'app/TemplateWizard/NEW_TEMPLATE_ADDED';
